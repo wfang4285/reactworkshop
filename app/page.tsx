d95eaebc483ac9a7098fb5a7a05dabd31d2b2c7d
aurora4285 <wfang6763@gmail.com>
2024-02-24 14:12:47 -0500

    Set up.
    React workshop starting set up.

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import MyComponent from "@/components/MyComponent/MyComponent";
+
+export default function Home() {
+  return (
+    <div>
+      <MyComponent />
+    </div>
+  );
+}
---
+++ components/MyComponent/MyComponent.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function MyComponent(){
+    return(
+        <h1>My Component</h1>
+    )
+}
